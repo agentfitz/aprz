f75d4060ecc5f12316500fd5b734f4b3ebf54a89
Brian FitzGerald <bmfitzgerald3@gmail.com>
2012-07-18 01:32:23 -0400

    initial commit

---
+++ index.php
@@ -0,0 +1,60 @@
+<!doctype html>
+
+
+<html>
+
+	<head>
+		<title>Apperize</title>
+		<link rel="stylesheet" type="text/css" href="lib/css/style.css" />
+		<link href='http://fonts.googleapis.com/css?family=Alike' rel='stylesheet' type='text/css' />
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
+	</head>
+
+	<body>
+		
+		<div id="headerOuter">
+		
+			<header id="headerInner">
+			
+				<img src="lib/img/apperize_logo.png" id="logo" />
+				
+				<nav>
+					<ul>
+						<li><a href="#">what</a></li>
+						<li><a href="#">tour</a></li>
+						<li><a href="#">get it</a></li>
+					</ul>
+				</nav>
+			
+				<img src="lib/img/home_page_icons.png" id="icons" />
+				<h1>Easily add your favorite websites as icons on your desktop!</h1>
+			
+			</header>			
+			
+		</div>
+		
+			<div id="mainOuter">
+			
+				<div id="mainInner">
+					
+					<div class="btn_container">
+						<div class="btn_container_inner">
+							<a href="#" class="btn large">Learn More</a>
+							<a href="#" class="btn large">Add to Cart</a>
+						</div>					
+					</div>
+					
+					<ul id="benefits_list">
+						<li>Quick access to the sites you love</li>
+						<li>Clean and clutter free desktop</li>
+						<li>Saves you time and typing</li>
+						<li>Beautiful icons without the hassle</li>
+					</ul>
+									
+				</div>
+			
+			</div>
+	
+	</body>
+
+</html>
